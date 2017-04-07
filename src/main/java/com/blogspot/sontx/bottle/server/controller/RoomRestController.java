86b4cbd95c6cb9fa6bda26f4c00c71da1c4c1b97
sontx <xuanson33bk@gmail.com>
2017-04-07 17:52:08 +0700

    - change url pattern for add message

--- src/main/java/com/blogspot/sontx/bottle/server/controller/RoomRestController.java
+++ src/main/java/com/blogspot/sontx/bottle/server/controller/RoomRestController.java
@@ -6,23 +6,21 @@ import com.blogspot.sontx.bottle.server.model.service.message.RoomMessageService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rest/messages/room")
-public class RoomMessageRestController {
+@RequestMapping("/rest/rooms")
+public class RoomRestController {
     private final RoomMessageService roomMessageService;
 
     @Autowired
-    public RoomMessageRestController(RoomMessageService roomMessageService) {
+    public RoomRestController(RoomMessageService roomMessageService) {
         this.roomMessageService = roomMessageService;
     }
 
-    @PostMapping
-    ResponseEntity postMessage(@RequestBody RoomMessage message, UsernamePasswordAuthenticationToken authenticationToken) {
+    @PostMapping("{roomId}/messages")
+    ResponseEntity postMessage(@PathVariable int roomId, @RequestBody RoomMessage message, UsernamePasswordAuthenticationToken authenticationToken) {
+        message.setRoomId(roomId);
         RoomMessage roomMessage = roomMessageService.postMessage(message, (AuthData) authenticationToken.getPrincipal());
         return roomMessage != null ? ResponseEntity.ok(roomMessage) : ResponseEntity.status(400).build();
     }
